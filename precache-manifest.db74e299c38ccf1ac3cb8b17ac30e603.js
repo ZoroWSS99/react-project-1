38849fc64db8ab1073d1ba68a45f4d26fedf34be
ZoroWSS99 <luiss.eduardovieira@gmail.com>
2022-10-17 18:16:46 -0300

    Updates

---
+++ precache-manifest.db74e299c38ccf1ac3cb8b17ac30e603.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d32cc31951f3c682a44",
+    "url": "/react-project-1/static/css/main.35d8bf92.chunk.css"
+  },
+  {
+    "revision": "4d32cc31951f3c682a44",
+    "url": "/react-project-1/static/js/main.4d32cc31.chunk.js"
+  },
+  {
+    "revision": "14cdfeb9851f0c31c2c6",
+    "url": "/react-project-1/static/js/1.14cdfeb9.chunk.js"
+  },
+  {
+    "revision": "b53f37fdea07d2838eaa",
+    "url": "/react-project-1/static/js/runtime~main.b53f37fd.js"
+  },
+  {
+    "revision": "0d35630dd19b2686ef6bf95093154af2",
+    "url": "/react-project-1/index.html"
+  }
+];
